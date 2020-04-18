bc6cc55d09bf22caa9f4fc63037ff08ef1a03703
Rubixdude7 <32314830+Rubixdude7@users.noreply.github.com>
2020-04-18 16:26:32 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace MyLibrary
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                string s = Console.ReadLine();
+                int a = Int32.Parse(s);
+                Console.WriteLine(Subfactorial(a));
+                Console.ReadKey();
+            }
+            catch (Exception) { }
+        }
+
+        public static int Subfactorial(int i)
+        {
+            if (i == 0) return 1;
+            else if (i == 1) return 0;
+            else
+            {
+                return (i - 1) * (Subfactorial(i - 1) + Subfactorial(i - 2));
+            }
+        }
+    }
+}
